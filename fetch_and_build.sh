74d490ad80ae9056f5abee0336c1088202b8289a
saeid rezaei <saeid.rezaei@unicorn.enginnering>
2025-04-15 15:54:08 +0200

    cross compile

--- fetch_and_build.sh
+++ fetch_and_build.sh
@@ -2,7 +2,7 @@
 
 # Set the absolute path to your cross-compilers
 #CROSS_COMPILE_PATH=/home/ubuntu/Tools/arm-none-linux-gnueabihf-10.02/bin/     # Modify this to the correct path
-CROSS_COMPILE=arm-none-linux-gnueabihf-  # Modify this if your toolchain has a different prefix
+#CROSS_COMPILE=arm-none-linux-gnueabihf-  # Modify this if your toolchain has a different prefix
 
 # Set the compilers and linker explicitly
 CC=${CROSS_COMPILE_PATH}${CROSS_COMPILE}gcc
